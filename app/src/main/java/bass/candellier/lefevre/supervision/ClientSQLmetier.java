4b512c1059e993ca700cb7648331411b72644453
Arckorus <lefevre.pierre.m.d@gmail.com>
2015-03-10 17:14:20 +0100

    Prem's

---
+++ app/src/main/java/bass/candellier/lefevre/supervision/ClientSQLmetier.java
@@ -0,0 +1,11 @@
+package bass.candellier.lefevre.supervision;
+
+import java.sql.Connection;
+
+public class ClientSQLmetier {
+
+    private String serveurBDD, nomBDD, userBDD, mdpBDD, portBDD, connexionStringBDD;
+    private Connection conn = null;
+
+    
+}
