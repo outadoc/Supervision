abd0b3873609e07058c3f4a8c9cff3d97f2c3e55
Arckorus <lefevre.pierre.m.d@gmail.com>
2015-03-10 17:24:32 +0100

    ajout getter setter clientsqlmetier

--- app/src/main/java/bass/candellier/lefevre/supervision/ClientSQLmetier.java
+++ app/src/main/java/bass/candellier/lefevre/supervision/ClientSQLmetier.java
@@ -3,9 +3,55 @@ package bass.candellier.lefevre.supervision;
 import java.sql.Connection;
 
 public class ClientSQLmetier {
-
+    private static final String TAG = "";
     private String serveurBDD, nomBDD, userBDD, mdpBDD, portBDD, connexionStringBDD;
     private Connection conn = null;
 
-    
+    public String getServeurBDD() {
+        return serveurBDD;
+    }
+
+    public void setServeurBDD(String serveurBDD) {
+        this.serveurBDD = serveurBDD;
+    }
+
+    public String getNomBDD() {
+        return nomBDD;
+    }
+
+    public void setNomBDD(String nomBDD) {
+        this.nomBDD = nomBDD;
+    }
+
+    public String getMdpBDD() {
+        return mdpBDD;
+    }
+
+    public void setMdpBDD(String mdpBDD) {
+        this.mdpBDD = mdpBDD;
+    }
+
+    public String getPortBDD() {
+        return portBDD;
+    }
+
+    public void setPortBDD(String portBDD) {
+        this.portBDD = portBDD;
+    }
+
+    public String getConnexionStringBDD() {
+        return connexionStringBDD;
+    }
+
+    public void setConnexionStringBDD(String connexionStringBDD) {
+        this.connexionStringBDD = connexionStringBDD;
+    }
+
+    public String getUserBDD() {
+        return userBDD;
+    }
+
+    public void setUserBDD(String userBDD) {
+        this.userBDD = userBDD;
+    }
 }
