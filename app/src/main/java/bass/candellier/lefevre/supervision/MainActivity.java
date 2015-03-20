9918fa14316397c34ea5cd0c3bfaf33d2afc4ef3
outadoc <outadoc@gmail.com>
2015-03-20 10:45:07 +0100

    Ajout des permissions nécessaires et récupération des éléments d'interface

--- app/src/main/java/bass/candellier/lefevre/supervision/MainActivity.java
+++ app/src/main/java/bass/candellier/lefevre/supervision/MainActivity.java
@@ -5,14 +5,31 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+	private TextView lblTemperatureBaie;
+	private TextView lblUtilisationDisque;
+	private GridLayout gridCpuUsage;
+
+	private ProgressBar progressBar;
+	private TextView lblProgressStatus;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		lblTemperatureBaie = (TextView) findViewById(R.id.lbl_temp_baie);
+		lblUtilisationDisque = (TextView) findViewById(R.id.lbl_util_disques);
+		gridCpuUsage = (GridLayout) findViewById(R.id.container_cpu_usage);
+
+		progressBar = (ProgressBar) findViewById(R.id.progress_status);
+		lblProgressStatus = (TextView) findViewById(R.id.lbl_progress_status);
 	}
 
 	@Override
@@ -32,4 +49,5 @@ public class MainActivity extends ActionBarActivity {
 
 		return super.onOptionsItemSelected(item);
 	}
+
 }
