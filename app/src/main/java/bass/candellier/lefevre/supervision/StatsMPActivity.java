1ead55ebec53d98d32e9e373a2e9725f2b455972
Ka3l <altair78200@aol.fr>
2015-03-24 14:44:16 +0100

    "ListView mp ajoutée maggle posay alley"

--- app/src/main/java/bass/candellier/lefevre/supervision/StatsMPActivity.java
+++ app/src/main/java/bass/candellier/lefevre/supervision/StatsMPActivity.java
@@ -2,12 +2,20 @@ package bass.candellier.lefevre.supervision;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class StatsMPActivity extends ActionBarActivity {
 
@@ -18,16 +26,19 @@ public class StatsMPActivity extends ActionBarActivity {
     private ProgressDialog progressDialog;
     private ProgressDialog dialogP;
     private ClientSQLmetier clientBDD;
+    private ArrayList<UsageMP> resRequete = new ArrayList<>();
+    private int n=10;
 
     public StatsMPActivity() {
     }
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         progressDialog = new ProgressDialog(this);
         progressDialog.setCanceledOnTouchOutside(false);
-        setContentView(R.layout.activity_stats_mp);
+        setContentView(R.layout.activity_stats_dd);
+        listeView = (ListView) findViewById(R.id.liste_stats_dd);
+        new Recuperation().execute();
     }
 
     @Override
@@ -37,7 +48,7 @@ public class StatsMPActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_stats_mp, menu);
+        ;
         return true;
     }
 
@@ -45,7 +56,7 @@ public class StatsMPActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_settings: {
-                startActivity(new Intent(this, PlotMPActivity.class)/*.putParcelableArrayListExtra("temp", resRequete)*/);
+
                 return true;
             }
         }
@@ -57,15 +68,52 @@ public class StatsMPActivity extends ActionBarActivity {
 
     }
 
-    public void printExceptionMessageOnUIThread(Exception e) {
+
+    public void addNewLigneTemp(Temp temp) {
 
     }
 
-    public void startThreadLectureUsageMP(ClientSQLmetier base) {
+    public class Recuperation extends AsyncTask<Void, Void, Void> {
+        ArrayAdapter<UsageDD> liste;
 
-    }
+        @Override
+        protected void onPreExecute() {
 
-    public void addNewLigneUsageMP(UsageMP temp) {
+        }
 
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                String ip = prefs.getString(PreferencesFragment.PREFKEY_HOSTNAME, "82.233.223.249");
+                String port = prefs.getString(PreferencesFragment.PREFKEY_PORT, "1433");
+                String username = prefs.getString(PreferencesFragment.PREFKEY_USERNAME, "supervision");
+                String password = prefs.getString(PreferencesFragment.PREFKEY_PASSWORD, "Password1234");
+
+                try {
+                    clientBDD = new ClientSQLmetier(ip, "Supervision", username, password, port, 10);
+                } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                ResultSet tab = clientBDD.getTableUsageMP(n);
+
+                while (tab.next()) {
+                    resRequete.add(new UsageMP(tab.getString("date"),tab.getInt("nbProcs"), tab.getInt("usageMP1"), tab.getInt("usageMP2"),tab.getInt("usageMP3"),tab.getInt("usageMP4"),tab.getInt("usageMP5"),tab.getInt("usageMP6"),tab.getInt("usageMP7"),tab.getInt("usageMP8")));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+
+
+            liste = new ArrayAdapter(StatsMPActivity.this, android.R.layout.simple_list_item_1, resRequete);
+            listeView.setAdapter(liste);
+            liste.notifyDataSetChanged();
+        }
     }
 }
