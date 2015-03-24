6d40d8bce2db98799366d8233d2de0ce0c948fe0
Arckorus <lefevre.pierre.m.d@gmail.com>
2015-03-24 16:39:49 +0100

    le graphique de l'usage du disque fonctionne

--- app/src/main/java/bass/candellier/lefevre/supervision/UsageDD.java
+++ app/src/main/java/bass/candellier/lefevre/supervision/UsageDD.java
@@ -1,16 +1,52 @@
 package bass.candellier.lefevre.supervision;
 
-public class UsageDD {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class UsageDD implements Parcelable {
     private String sdate;
     private String Usage;
     private long capacite;
     private long utilisation;
+    public static final Parcelable.Creator<UsageDD> CREATOR = new Parcelable.Creator<UsageDD>() {
+        public UsageDD createFromParcel(Parcel in) {
+            return new UsageDD(in);
+        }
+
+        public UsageDD[] newArray(int size) {
+            return new UsageDD[size];
+        }
+    };
 
     public UsageDD(String sdate, String usage, long capacite, long utilisation) {
         this.sdate = sdate;
         Usage = usage;
         this.capacite = capacite;
         this.utilisation = utilisation;
+    }
+
+    public UsageDD(Parcel in) {
+        readFromParcel(in);
+    }
+
+    private void readFromParcel(Parcel in) {
+        this.sdate = in.readString();
+        this.Usage = in.readString();
+        this.capacite = in.readLong();
+        this.utilisation = in.readLong();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.sdate);
+        dest.writeString(this.Usage);
+        dest.writeLong(this.capacite);
+        dest.writeLong(this.utilisation);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
     public String getSdate() {
